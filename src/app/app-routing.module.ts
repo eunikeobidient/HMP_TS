922f2be3c422af6eaf76bf420f0daedfbe7d0663
SoenHizkia <soenhizkia86@gmail.com>
2026-09-12 13:44:37 +0700

    [160424017-SoenHizkia] Membuat Page Detail Produk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'detail-produk',
+    loadChildren: () => import('./detail-produk/detail-produk.module').then( m => m.DetailProdukPageModule)
   },
+
 ];
 
 @NgModule({
